9701da29293fe7f2c27b0b00f716c1003e742f43
gk646 <gk646@proton.me>
2023-09-10 21:06:32 +0200

    added monster and projectile drawing to minimap and region map added thread sync to both

--- src/ui/game/RegionMap.h
+++ src/ui/game/RegionMap.h
@@ -2,10 +2,10 @@
 #define MAGEQUEST_SRC_UI_GAME_REGIONMAP_H_
 
 struct RegionMap final : public Window {
-  int zoom = 5;
+  float zoom = 5;
   bool dragged = false;
   PointI tile_offset{};
-  Vector2 last_mouse_pos;
+  Vector2 last_mouse_pos{};
   RegionMap()
       : Window(100, 100, SCREEN_WIDTH * 0.9, SCREEN_HEIGHT * 0.9, 20, "Region Map",
                KEY_M) {}
@@ -39,46 +39,51 @@ struct RegionMap final : public Window {
   }
   inline void draw_region_map() noexcept {
     auto player_tile = *PLAYER_TILE;
+
     if (!dragged) {
-      tile_offset = *PLAYER_TILE;
+      tile_offset = player_tile;
     }
+    int curr_size = CURRENT_MAP_SIZE;
+    float base_x = whole_window.x + whole_window.width / 2 - tile_offset.x * zoom;
+    float base_y = whole_window.y + whole_window.height / 2 - tile_offset.y * zoom;
+    float draw_x, draw_y;
+    for (int_fast32_t i = 0; i < curr_size; i++) {
+      for (int_fast32_t j = 0; j < curr_size; j++) {
+        draw_x = base_x + i * zoom;
+        draw_y = base_y + j * zoom;
 
-    int draw_x = whole_window.x + whole_window.width / 2 - tile_offset.x * zoom;
-    int draw_y = whole_window.y + whole_window.height / 2 - tile_offset.y * zoom;
-
-    for (int_fast32_t i = 0; i < CURRENT_MAP_SIZE; i++) {
-      for (int_fast32_t j = 0; j < CURRENT_MAP_SIZE; j++) {
-        if (MiniMap::map_bounds(i, j)) {
+        if (window_bounds(draw_x, draw_y)) [[likely]] {
           if (COLLISIONS[CURRENT_BACK_GROUND[i][j]] == C_SOLID ||
               COLLISIONS[CURRENT_MIDDLE_GROUND[i][j]] == C_SOLID) {
-            if (window_bounds(draw_x + i * zoom, draw_y + j * zoom)) {
-
-              DrawRectangleProFast(draw_x + i * zoom, draw_y + j * zoom, zoom,
-                                   Colors::darkBackground);
-            }
-          } else [[likely]] {
-            if (i == player_tile.x && j == player_tile.y) {
-              if (window_bounds(draw_x + i * zoom, draw_y + j * zoom)) {
-                DrawRectangleProFast(draw_x + i * zoom, draw_y + j * zoom, zoom,
-                                     Colors::Blue);
-              }
-            } else {
-              if (window_bounds(draw_x + i * zoom, draw_y + j * zoom)) {
-                DrawRectangleProFast(draw_x + i * zoom, draw_y + j * zoom, zoom,
-                                     Colors::map_green);
-              }
-            }
-          }
-        } else {
-          if (window_bounds(draw_x + i * zoom, draw_y + j * zoom)) {
-            DrawRectangleProFast(draw_x + i * zoom, draw_y + j * zoom, zoom,
-                                 Colors::lightGreyMiddleAlpha);
+            DrawRectangleProFast(draw_x, draw_y, zoom, Colors::darkBackground);
+          } else if (i == player_tile.x && j == player_tile.y) {
+            DrawRectangleProFast(draw_x, draw_y, zoom, Colors::Blue);
+          } else {
+            DrawRectangleProFast(draw_x, draw_y, zoom, Colors::map_green);
           }
         }
       }
     }
+    std::shared_lock<std::shared_mutex> lock(rwLock);
+    for (const auto monster : MONSTERS) {
+      if (monster->tile_pos.dist(player_tile) < 20) {
+        DrawRectangleProFast(base_x + monster->tile_pos.x * zoom,
+                             base_y + monster->tile_pos.y * zoom, zoom, Colors::Red);
+      }
+    }
+    for (const auto projectile : PROJECTILES) {
+      if(projectile->from_player){
+        DrawRectangleProFast(base_x + projectile->tile_pos.x  * zoom,
+                             base_y + projectile->tile_pos.y  * zoom, 3,
+                             Colors::Blue);
+      }else{
+        DrawRectangleProFast(base_x + projectile->tile_pos.x  * zoom,
+                             base_y + projectile->tile_pos.y  * zoom, 3,
+                             Colors::Red);
+      }
+    }
   }
-  inline bool window_bounds(int x, int y) noexcept {
+  inline bool window_bounds(float x, float y) const noexcept {
     return x >= whole_window.x && x < whole_window.x + whole_window.width &&
            y >= whole_window.y && y < whole_window.y + whole_window.height;
   }
@@ -97,7 +102,7 @@ struct RegionMap final : public Window {
     last_mouse_pos = mouse_pos;
 
     int wheel = GetMouseWheelMove();
-    if (zoom + wheel > 0) {
+    if (zoom + wheel > 0 && zoom + wheel < 14) {
       zoom += wheel;
     }
   }
@@ -106,8 +111,6 @@ struct RegionMap final : public Window {
     if (whole_window.width < SCREEN_WIDTH * 0.9) {
       whole_window.width = SCREEN_WIDTH * 0.9;
       header_bar.width = SCREEN_WIDTH * 0.9;
-    }
-    if (whole_window.height < SCREEN_HEIGHT * 0.9) {
       whole_window.height = SCREEN_HEIGHT * 0.9;
     }
   }
