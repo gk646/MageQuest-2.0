aa68d5a25b5474dfc7b2332e77201dc2a8dc5454
gk646 <gk646@proton.me>
2023-10-31 17:30:33 +0100

    fixed ScrollPane.h

--- src/ui/elements/Content.h
+++ src/ui/elements/Content.h
@@ -3,10 +3,10 @@
 //Defines something as possible content of another class (e.g. ScrollPane)
 struct Content {
   //Passes the bounds of the container with the request respect them
-  inline virtual void Draw(RectangleR bounds) noexcept = 0;
+  inline virtual void Draw(RectangleR bounds, float scrollOffset) noexcept = 0;
   //Called on the update thread
   inline virtual void Update() noexcept = 0;
-  inline virtual int GetWidth() const noexcept = 0;
-  inline virtual int GetHeight() const noexcept = 0;
+  inline virtual float GetWidth() const noexcept = 0;
+  inline virtual float GetHeight() const noexcept = 0;
 };
 #endif  //MAGEQUEST_SRC_UI_ELEMENTS_CONTENT_H_
